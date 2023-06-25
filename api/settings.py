20b73aaffbcac481836b19e82b164931badcb0da
dabarov <simlyalde@gmail.com>
2023-06-25 22:14:22 +0200

    :heavy_plus_sign: Fix redis dependency

--- api/settings.py
+++ api/settings.py
@@ -126,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery settings
+
+# set the celery broker url
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+  
+# set the celery result backend
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
+  
+# set the celery timezone
+CELERY_TIMEZONE = 'UTC'
